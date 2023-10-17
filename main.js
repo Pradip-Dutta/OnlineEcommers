e5f748a4e8855daf2b417b509875ea475a3950c9
Pradip <91416089+Pradip-Dutta@users.noreply.github.com>
2023-10-17 19:55:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+const cartNotification = document.getElementById('cart-notification');
 
+let cartCount = 0;
+
+addToCartButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    cartCount++;
+    cartNotification.textContent = cartCount;
+  });
+});
